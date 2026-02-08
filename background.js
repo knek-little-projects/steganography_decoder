da878fedeb2f2e001d96a1f085bcea9778423040
yoba <yobapochta@gmail.com>
2026-02-08 09:26:31 -0500

    -

--- background.js
+++ background.js
@@ -4,23 +4,4 @@ chrome.action.onClicked.addListener(() => {
   });
 });
 
-// Создаем пункт контекстного меню при установке расширения
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'decodeImage',
-    title: 'Decode steganography',
-    contexts: ['image']
-  });
-});
-
-// Обработчик клика по пункту контекстного меню
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'decodeImage' && info.srcUrl) {
-    // Открываем декодер с URL изображения
-    chrome.tabs.create({
-      url: chrome.runtime.getURL(`decoder.html?imageUrl=${encodeURIComponent(info.srcUrl)}`)
-    });
-  }
-});
-
 
