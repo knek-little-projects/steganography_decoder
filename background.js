2bb1d2a5cd6e560cb612e58b2812a835b7b9ae41
yoba <yobapochta@gmail.com>
2026-02-08 09:32:30 -0500

    -

--- background.js
+++ background.js
@@ -4,4 +4,12 @@ chrome.action.onClicked.addListener(() => {
   });
 });
 
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.tabs.create({
+      url: "https://knek-little-projects.github.io/steganography_decoder/welcome/",
+    });
+  }
+});
+
 
